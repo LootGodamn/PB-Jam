90e7ff128d76d556e6e66ac22b8443b8cdf0eaaa
Geek_Josytick <matys.gueroultromon@gmail.com>
2020-10-17 17:52:49 +0200

    Potato Man Player
    Added basic potato man player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,25 +10,47 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -20f;
     public float jumpHeight = 2f;
 
+    float currSpeed;
+    float currJumpHeight;
+
     public Transform groundCheck;
-    public float groundDistance = 0.1f;
+    public float groundDistance = 0.2f;
     public LayerMask groundMask;
 
+    bool potatoed;
+
     Vector3 velocity;
-    bool isGrounded;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currSpeed = speed;
+        currJumpHeight = jumpHeight;
+    }
+
+    // Function called upon potatification
+    void PotateOn(){
+        potatoed = true;
+        currSpeed = speed*1.5f;
+        currJumpHeight = jumpHeight*1.5f;
+        transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        controller.Move(new Vector3(0, 2f, 0));
+    }
+
+    void PotateOff(){
+        potatoed = false;
+        currSpeed = speed;
+        currJumpHeight = jumpHeight;
+        transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0){
+        if (controller.isGrounded && velocity.y < 0){
             velocity.y = -2f;
         }
 
@@ -36,10 +58,17 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right*x+transform.forward*z;
-        controller.Move(move*speed*Time.deltaTime);
+        controller.Move(move*currSpeed*Time.deltaTime);
 
-        if (Input.GetAxis("Jump") != 0 && isGrounded){
-            velocity.y = jumpHeight;
+        if (Input.GetKeyDown("e") && controller.isGrounded){
+            velocity.y = currJumpHeight;
+        }
+
+        if (Input.GetKeyDown("f")){
+            if (potatoed)
+                PotateOff();
+            else
+                PotateOn();
         }
 
         velocity.y += gravity*Time.deltaTime;
